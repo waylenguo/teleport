5baaf16b9ccdc0c74c7e34a3db3e72e6468a243f
Tim Ross <tim.ross@goteleport.com>
2022-02-11 14:08:29 -0500

    fix linter and unit test failure

--- api/client/client.go
+++ api/client/client.go
@@ -20,6 +20,7 @@ import (
 	"compress/gzip"
 	"context"
 	"crypto/tls"
+	"errors"
 	"fmt"
 	"io"
 	"net"
@@ -440,6 +441,9 @@ func (c *Client) grpcDialer() func(ctx context.Context, addr string) (net.Conn, 
 // alongside the DialInBackground client config option to wait until background dialing has completed.
 func (c *Client) waitForConnectionReady(ctx context.Context) error {
 	for {
+		if c.conn == nil {
+			return errors.New("conn was closed")
+		}
 		switch state := c.conn.GetState(); state {
 		case connectivity.Ready:
 			return nil
